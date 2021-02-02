647dca950322b8d25d5e794bb67b92721b13566e
Katerina <psyckalla.gmail.com>
2021-02-02 16:39:41 -0500

    fixed throttle

--- src/app.js
+++ src/app.js
@@ -11,14 +11,25 @@ const {
   ownerRouter,
   logRouter,
 } = require("./endpoints/endpoints-router");
+const requestIp = require("request-ip");
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 15 minutes
+  max: 2, // limit each IP to 100 requests per windowMs
+});
 
 const app = express();
+// app.use(limiter);
+// app.use("/api/first/", limiter);
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use(requestIp.mw());
 
 app.use(registrationRouter);
 app.use(eventsRouter);
--- src/endpoints/endpoints-router.js
+++ src/endpoints/endpoints-router.js
@@ -16,6 +16,21 @@ const OwnerService = require("../services/owner-service");
 registrationRouter
   .route("/api/first/registration")
   .post(jsonParser, async (req, res, next) => {
+    console.log(req.ip, "oneip");
+    console.log(req.connection.remoteAddress, "twoip");
+    var ip =
+      req.headers["x-forwarded-for"] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+    console.log(ip, "threeip");
+
+    const ip1 = req.clientIp;
+
+    console.log(ip1, "fourip");
+
+    // console.log(req.headers);
     ValidationServices.validate(req.body.data);
     FirstTimeRegistrationService.first(req.body.data);
 
